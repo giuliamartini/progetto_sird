9169395beb14dd7581275f8a278fc0921672d3f0
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2022-11-28 11:33:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,9 +16,9 @@ void print_f(std::ostream& os,
   os << clear;
   os << "\t\t\t" << '+' << std::string(N * 3 - 2 * infectivity, '-') << "+\n";
 
-  for (int r = infectivity; r < N - infectivity; ++r) {
+  for (int r = 0; r < N; ++r) {
     os << "\t\t\t" << '|';
-    for (int c = infectivity; c < 3 * N - infectivity; ++c) {
+    for (int c = 0; c < 3 * N; ++c) {
       Sird::Person const& person = population.person(r, c);
       switch (person) {
         case Sird::Person::Infected:
@@ -47,14 +47,15 @@ void print_f(std::ostream& os,
     }
     os << "|\n";
   }
-  os << "\t\t\t" << '+' << std::string(N * 3 - 2 * infectivity, '-') << "+\n";
+  os << "\t\t\t" << '+' << std::string(N * 3, '-') << "+\n";
 }
 
 int main()
 {
-  // std::cout << "Please enter the value of beta" << '\n';
-  double beta = 0.8;
-  // std::cin >> beta;
+  std::cout << "Please enter the value of beta" << '\n';
+  double beta;
+  //= 0.8;
+  std::cin >> beta;
   // std::cout << '\n' << "Please enter the value of gamma " << '\n';
   double gamma = 0.3;
   // std::cin >> gamma;
@@ -62,54 +63,53 @@ int main()
   double delta = 0.05;
   //  std::cin >> delta;
   // std::cout << "Please enter the value of infectivity" << '\n';
+  // std::cout << "Please enter epsilon" << '\n';
+  double epsilon = 0.7;
+  // std::cin >> epsilon;
+  //= 0.7;
   int infectivity = 3;
   // std::cin >> infectivity;
-  // std::cout << "Please enter the number of people, less than 7500" << '\n';
+  // std::cout << "Please enter the number of people, less than n*n*3" << '\n';
   int numberOfPeople
 
       = 4000;
   // std::cin >> numberOfPeople;
-  // std::cout << "Please enter epsilon" << '\n';
+  // std::cout  << "Please enter the dimension of the board" << '\n';
+  constexpr int n_ = 50;
+  // std::cin >> n_;
 
-  double epsilon = 0.7;
-  // std::cin >> epsilon;
-  //= 0.7;
   //  numero di persone massimo che si possono vaccinare in un giorno
   // std::cout << "Please enter the number numero massimo di vaccini al giorno"
   //         << '\n';
   int toBeVaccinated = 20;
   // std::cin >> toBeVaccinated;
 
-  constexpr int board_size = 50;
-  Sird::Population board(board_size);
+  // constexpr int board_size = n_;
+  Sird::Population board(n_);
   board.Sird::Population::placePeople(numberOfPeople, infectivity);
 
   board.Sird::Population::setInfected(
-      {25, 75});  // pazienti 0 al centro della board
-  board.Sird::Population::setInfected({25, 74});
-  board.Sird::Population::setInfected({25, 73});
-  board.Sird::Population::setInfected({24, 75});
-  board.Sird::Population::setInfected({26, 75});
+      {n_ / 2, n_ / 2 * 3});  // pazienti 0 al centro della board
+  board.Sird::Population::setInfected({n_ / 2, n_ / 2 * 3 - 1});
+  board.Sird::Population::setInfected({n_ / 2, n_ / 2 * 3 + 1});
+  board.Sird::Population::setInfected({n_ / 2 + 1, n_ / 2 * 3});
+  board.Sird::Population::setInfected({n_ / 2 - 1, n_ / 2 * 3});
   print_f(std::cout, board, infectivity);
   int frame = 0;
-
+  Sird::State initialSituation = board.counts(infectivity);
+  int init = initialSituation.S + initialSituation.I;
   // definizione grafico
   std::ofstream graph;
-  graph << "graph\n";
-  graph.open("graph.csv");
+  graph << "Graph\n";
+  graph.open("Graph.csv");
   graph << "Day,Susceptible,Infected,Recovered,Dead,Vaccinated\n";
 
   // int infects = board.Sird::Population::count(infectivity).I;
   Sird::State count{0, 0, 0, 0, 0};
-  Sird::State total{0, 0, 0, 0, 0};
+
   while (board.Sird::Population::counts(infectivity).I != 0) {
     count = board.counts(infectivity);
-    /*  total = {total.S += count.S,
-               total.I += count.I,
-               total.R += count.R,
-               total.D += count.D,
-               total.V += count.V};*/
-
+    assert(count.S + count.I + count.R + count.D + count.V == init);
     print_f(std::cout, board, infectivity);
     std::cout << "S: "
               << count.S + count.V
@@ -122,8 +122,6 @@ int main()
           << count.D << "," << count.V << '\n';
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     //  std::chrono_literals::std::this_thread::sleep_for(10ms);
-
-    // system("clear");
 
     board.evolve(infectivity, beta, gamma, delta, epsilon);
     board.move(infectivity);
@@ -142,8 +140,7 @@ int main()
       board.vaccinationOver(infectivity, gamma, count);
     }*/
   }
-  /* std::cout << "total Infected from the begining of the pandemic: -I: "
-             << total.I << " - D: " << total.D << " - V: " << total.V << '\n';*/
+  system("clear");
   graph.close();
   system("python ./graph.py");
 }
