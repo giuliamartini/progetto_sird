31484f782073b75baa852c994194b041e0a5b5e6
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2022-11-29 13:07:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <chrono>
+#include <exception>
 #include <fstream>
 #include <iomanip>
 #include <thread>
+
 #include "sird.hpp"
 
 void print_f(std::ostream& os,
@@ -14,17 +17,18 @@ void print_f(std::ostream& os,
   int const N = population.side();
 
   os << clear;
-  os << "\t\t\t" << '+' << std::string(N * 3 - 2 * infectivity, '-') << "+\n";
+  os << "\t\t\t" << '+' << std::string(N * 3 - 2 - infectivity, '-') << "+\n";
 
-  for (int r = 0; r < N; ++r) {
+  for (int r = infectivity; r < N - infectivity + 1; ++r) {
     os << "\t\t\t" << '|';
-    for (int c = 0; c < 3 * N; ++c) {
+    for (int c = infectivity; c < 3 * N - infectivity + 1; ++c) {
       Sird::Person const& person = population.person(r, c);
       switch (person) {
         case Sird::Person::Infected:
           os << "\033[31m*\033[0m";
           break;
         case Sird::Person::Vaccinated:
+          // os << '^';
           os << "\033[32m^\033[0m";
           break;
         case Sird::Person::Susceptible:
@@ -46,75 +50,112 @@ void print_f(std::ostream& os,
     }
     os << "|\n";
   }
-  os << "\t\t\t" << '+' << std::string(N * 3, '-') << "+\n";
+  os << "\t\t\t" << '+' << std::string(N * 3 - 2 - infectivity, '-') << "+\n";
 }
 
 int main()
 {
-  std::cout << "Please enter the value of beta, make sure it is between 0.0 and 1.0" << '\n';
+  std::cout
+      << "Please enter the value of beta, make sure it is between 0.0 and 1.0"
+      << '\n';
   double beta;
+
   std::cin >> beta;
-  std::cout << '\n' << "Please enter the value of gamma, make sure it is between 0.0 and 1.0" << '\n';
+  if (beta < 0 || beta > 1) {
+    throw std::runtime_error{"the value of beta is not allowed \n"};
+  };
+  std::cout
+      << '\n'
+      << "Please enter the value of gamma, make sure it is between 0.0 and 1.0"
+      << '\n';
   double gamma;
   std::cin >> gamma;
-  std::cout << '\n' << "Please enter the value of delta, make sure it is between 0.0 and 1.0" << '\n';
+  if (gamma < 0 || gamma > 1) {
+    throw std::runtime_error{"the value of gamma is not allowed \n"};
+  };
+  std::cout
+      << '\n'
+      << "Please enter the value of delta, make sure it is between 0.0 and 1.0"
+      << '\n';
   double delta;
   std::cin >> delta;
-  std::cout << '\n' << "Please enter the value of epsilon, make sure it is between 0.0 and 1.0" << '\n';
+  if (delta < 0 || delta > 1) {
+    throw std::runtime_error{"the value of delta is not allowed \n"};
+  };
+  std::cout << '\n'
+            << "Please enter the value of epsilon, make sure it is between 0.0 "
+               "and 1.0"
+            << '\n';
   double epsilon;
+
   std::cin >> epsilon;
-  std::cout << '\n' << "Please enter the value of infectivity (i)" << '\n';
-  int infectivity; 
-  std::cin >> infectivity;
-  std::cout  << '\n' << "Please enter the dimension of the board" << '\n';
+  if (epsilon < 0 || epsilon > 1) {
+    throw std::runtime_error{"the value of epsilon is not allowed \n"};
+  };
+  std::cout << '\n'
+            << "Please enter the dimension of the board (good choice between "
+               "20 and 50)"
+            << '\n';
   int n_;
   std::cin >> n_;
-  std::cout << '\n' << "Please enter the initial number of susceptibles, make sure it is less than 3 * (n - i) * (n - i)" << '\n';
+  std::cout << '\n'
+            << "Please enter the value of infectivity (i), make sure it is "
+               "less than 5"
+            << '\n';
+  int infectivity;
+  std::cin >> infectivity;
+  if (infectivity > 10) {
+    throw std::runtime_error{"the value of infectivity is not allowed \n"};
+  };
+
+  std::cout << '\n'
+            << "Please enter the initial number of susceptibles, make sure it "
+               "is less than 3 * (n - i) * (n - i)"
+            << '\n';
   int numberOfPeople;
   std::cin >> numberOfPeople;
-  
+  if (numberOfPeople > 3 * (n_ - infectivity) * (n_ - infectivity)) {
+    throw std::runtime_error{"the number of people is not allowed \n"};
+  };
+
   //  numero di persone massimo che si possono vaccinare in un giorno
   int toBeVaccinated = 20;
 
-  // constexpr int board_size = n_;
   Sird::Population board(n_);
   board.placePeople(numberOfPeople, infectivity);
 
-  board.setInfected(
-      {n_ / 2, n_ / 2 * 3});  // pazienti 0 al centro della board
+  board.setInfected({n_ / 2, n_ / 2 * 3});  // pazienti 0 al centro della board
   board.setInfected({n_ / 2, n_ / 2 * 3 - 1});
   board.setInfected({n_ / 2, n_ / 2 * 3 + 1});
   board.setInfected({n_ / 2 + 1, n_ / 2 * 3});
   board.setInfected({n_ / 2 - 1, n_ / 2 * 3});
   print_f(std::cout, board, infectivity);
   int frame = 0;
-  Sird::State initialSituation = board.counts(infectivity);
+  Sird::State initialSituation = board.counts();
   int init = initialSituation.S + initialSituation.I;
-  // definizione grafico
+
+  // Grafico
+
   std::ofstream graph;
   graph << "Graph\n";
   graph.open("Graph.csv");
   graph << "Day,Susceptible,Infected,Recovered,Dead,Vaccinated\n";
-
-  // int infects = board.Sird::Population::count(infectivity).I;
   Sird::State count{0, 0, 0, 0, 0};
 
-  while (board.counts(infectivity).I != 0) {
-    count = board.counts(infectivity);
+  while (board.counts().I != 0) {
+    count = board.counts();
     assert(count.S + count.I + count.R + count.D + count.V == init);
     print_f(std::cout, board, infectivity);
     std::cout << "S: "
               << count.S + count.V
-              // tra i suscettibili conto anche i vaccinati
-              << " - I: " << count.I << " - R: " << count.R << " - D: " << count.D
-              << " - V: " << count.V << "\n\n";
+              // tra i Susceptibles si contano anche i vaccinati
+              << " - I: " << count.I << " - R: " << count.R
+              << " - D: " << count.D << " - V: " << count.V << "\n\n";
 
     std::cout << "Day " << frame << '\n';
     graph << frame << "," << count.S << "," << count.I << "," << count.R << ","
           << count.D << "," << count.V << '\n';
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    //  std::chrono_literals::std::this_thread::sleep_for(10ms);
-
     board.evolve(infectivity, beta, gamma, delta, epsilon);
     board.move(infectivity);
     ++frame;
@@ -125,12 +166,9 @@ int main()
                              epsilon)) {  // dopo 20 giorni partono i vaccini
       board.massVaccination(infectivity, toBeVaccinated, epsilon);
       if (frame >= 40) {
-        board.resusceptible(infectivity, 0.1, count);
+        board.susceptibleAgain(infectivity, 0.1, count);
       }  // dopo 40 giorni cominciano a tornare susceptible
     }
-    /*if (frame >= 50) {  // dopo 50 giorni i vaccini non fanno più effetto
-      board.vaccinationOver(infectivity, gamma, count);
-    }*/
   }
   system("clear");
   graph.close();
