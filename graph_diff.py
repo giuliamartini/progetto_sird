49bfee8faa61bc28f48fb6cb54e8626c13c221ac
giuliamartini <118760109+giuliamartini@users.noreply.github.com>
2022-12-06 20:11:14 +0100

    Update graph_diff.py

--- graph_diff.py
+++ graph_diff.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 headers = ['Day', 'Susceptible', 'Infected', 'Recovered']
-df = pd.read_csv('/home/giulia/plague3/graph_diff.csv')
+df = pd.read_csv('/home/giulia/progetto_sird-main/graph_diff.csv')
 # print(df)
 Day = df['Day']
 S = df['Susceptible']
